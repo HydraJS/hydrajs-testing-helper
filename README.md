f6cb2513fbb1ab2e8b3613cbbc1c357941e1a022
tcorral <amischol@gmail.com>
2014-01-02 12:22:32 +0100

    v 1.0.0 First revision

--- README.md
+++ README.md
@@ -1,4 +1,82 @@
-TestingHelper
-=============
+# hydrajs-testing-helper
+Is a testing extension helper that add some helper methods to make easy test all Hydra modules.
+## Requirements
+You will need to load it after the import of Hydra.js in your code.
 
-Testing helper tools to improve testability 
+## Install
+
+Install with [Bower](http://bower.io)
+
+    bower install hydrajs-testing-helper
+
+Install with [Component](http://component.io)
+
+    component install hydrajs-testing-helper
+
+Install with [NPM](http://npmjs.org)
+
+    npm install hydrajs-testing-helper
+
+### Use in browser
+
+Insert in your html code:
+
+	<script type="text/javascript" src="hydra.js"></script
+	<script type="text/javascript" src="hydrajs-testing-helper.js"></script>
+
+### Use with requirejs
+
+Insert in your code:
+
+    define(['hydrajs-testing-helper'], function ( testingHelper ) {
+        // code here.
+    });
+
+### Common usage
+
+hydrajs-testing-helper add some methods to Hydra.js library.
+
+## Hydra.setTestFramework
+
+*This method is needed to set which test framework we will use. If the test framework is not set the other methods will not work.*
+
+	Hydra.setTestFramework( oTestFramework );
+
+## Hydra.module.getModule
+
+This method returns a instance of the module is useful to use it when you want a module to be accessible in your tests.
+
+	Hydra.module.getModule( sModuleId, sIdInstance );
+
+## Hydra.module.test
+
+This method returns a module that will not wrap any method allowing you to test it and get the errors.
+
+	Hydra.module.test( sModuleId, function (oModule) {
+		//Here you will have the not wrapped module to be used in your test
+	});
+
+If you need to manage dependencies in a module you can use Hydra.module.test in this way:
+
+    var oModule = Hydra.module.test( sModuleId, [ dependency1, dependency2 ]);
+
+TIP: You could set a variable where save the oModule in this way.
+
+	var oModule = null;
+	Hydra.module.test( 'moduleId', function(oMod) {
+		oModule = oMod;
+	});
+
+## API
+### Hydra
+#### setTestFramework - Params [Object - framework test]
+This method expects an object [Jasmine, jstestdriver...]
+
+### Hydra.module
+#### getModule - Params [String - identifier of Module, String - identifier of Instance]
+#### test - Params [String - identifier of Module, [Function or Array]]
+
+*Tip: You can see how it can be used in the [Hydra's test file](https://github.com/HydraJS/HydraJS/blob/master/test/Hydra.js)*
+
+# License
+hydrajs-testing-helper is licensed under MIT license. (see LICENSE file)
---
+++ specs/hydrajs-testing-helperSpec.js
@@ -0,0 +1,92 @@
+describe( 'Hydra setTestFramework', function () {
+
+  it( 'should check that setTestFramework exist in Hydra.js', function () {
+    expect( Hydra.setTestFramework ).toBeDefined();
+  } );
+
+  it( 'should check that setTestFramework expects one argument', function () {
+    expect( Hydra.setTestFramework.length ).toEqual( 1 );
+  } );
+
+} );
+
+describe( 'Hydra getModule', function () {
+
+  beforeEach( function () {
+    sinon.stub( Hydra.module, 'getInstance' );
+  } );
+
+  afterEach( function () {
+    Hydra.module.getInstance.restore();
+    Hydra.setTestFramework(null);
+  } );
+
+  it( 'should throw an error if the module to get the instance has not been registered', function () {
+    expect( function () {
+      Hydra.setTestFramework( jasmine );
+
+      Hydra.module.getModule( 'test', 'test2' );
+    } ).toThrow();
+  } );
+
+  it( 'should check that Hydra.module.getInstance is called if the framework of test has been set', function () {
+    Hydra.setTestFramework( jasmine );
+
+    Hydra.module.register( 'test', function () {
+      return {};
+    } );
+
+    Hydra.module.getModule( 'test', 'test2' );
+
+    expect( Hydra.module.getInstance.callCount ).toEqual( 1 );
+  } );
+
+  it( 'should check that Hydra.module.getInstance is not called if the framework of test has not been set', function () {
+    Hydra.module.register('test', function(action)
+    {
+      return {};
+    });
+
+    Hydra.module.getModule( 'test', 'test2' );
+
+    expect( Hydra.module.getInstance.callCount ).toEqual( 0 );
+  } );
+} );
+
+describe( 'Hydra module test', function () {
+
+  var sModuleId = 'test'
+    , fpCallback = sinon.stub()
+    , fpDestroyStub = sinon.stub()
+    , fpModuleCreator = function () {
+      return {
+        init: function () {},
+        handleAction: function () {},
+        destroy: fpDestroyStub
+      };
+    };
+  beforeEach( function () {
+    var self = this;
+    Hydra.setTestFramework( jasmine );
+    Hydra.module.register( sModuleId, fpModuleCreator );
+  } );
+
+  afterEach( function () {
+    Hydra.module.remove( sModuleId );
+    Hydra.setTestFramework( null );
+  } );
+
+  it( 'should call the callback', function () {
+    Hydra.module.test( sModuleId, fpCallback );
+
+    expect( fpCallback.callCount ).toEqual( 1 );
+  } );
+
+  it( 'should return the module if test does not get a callback', function () {
+    var oModule = Hydra.module.test( sModuleId );
+
+    expect( oModule.init ).toBeDefined();
+    expect( typeof oModule.init ).toEqual( 'function' );
+  } );
+
+} );
